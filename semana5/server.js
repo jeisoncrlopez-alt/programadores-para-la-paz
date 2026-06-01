1e45b040e5c9ef082bd881fe3f911f43bc5dcd22
jeison stiven usuga lopez <jeisoncrlopez@gmail.com>
2026-05-31 20:50:25 -0500

    actividad semana 5

---
+++ semana5/server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+
+// Middleware para que el servidor pueda entender datos en formato JSON
+app.use(express.json());
+
+// Ruta raíz (GET)
+app.get('/', (req, res) => {
+  res.send('Servidor activo');
+});
+
+// Ruta de saludo (GET)
+app.get('/saludo', (req, res) => {
+  res.send('Hola comunidad');
+});
+
+// Ruta con parámetros (GET)
+app.get('/mensaje/:nombre', (req, res) => {
+  const nombre = req.params.nombre;
+  res.send('Hola ' + nombre);
+});
+
+// Configuración del puerto 3000
+app.listen(3000, () => {
+  console.log('Servidor ejecutándose en puerto 3000');
+});
